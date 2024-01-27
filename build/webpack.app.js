0ce25960602b86e0fcfbf602b0d2c15cb9bb76c6
Slava Abakumov <slava.abakumov@gmail.com>
2024-01-27 15:44:39 +0100

    update libraries.
    App build is working, tests build is failing. Not sure what to do with them right now.

--- build/webpack.app.js
+++ build/webpack.app.js
@@ -1,19 +1,19 @@
-var path = require('path');
-var base = require('./webpack.base');
+var path = require( 'path' );
+var base = require( './webpack.base' );
 
-module.exports = Object.assign(base, {
-	entry: path.resolve(__dirname, '../app/main.js'),
+module.exports = Object.assign( base, {
+	entry: path.resolve( __dirname, '../app/main.js' ),
 	output: {
-		path: path.resolve(__dirname, '../app/dist'),
+		path: path.resolve( __dirname, '../app/dist' ),
 		publicPath: '/dist/',
-		filename: 'app.js'
+		filename: 'app.js',
 	},
 	devServer: {
 		contentBase: 'app',
 		historyApiFallback: true,
-		noInfo: true
+		noInfo: true,
 	},
 	performance: {
-		hints: false
-	}
-});
+		hints: false,
+	},
+} );
--- build/webpack.base.js
+++ build/webpack.base.js
@@ -1,63 +1,69 @@
-var path = require('path');
-var webpack = require('webpack');
+var path = require( 'path' );
+var webpack = require( 'webpack' );
+const VueLoaderPlugin = require( 'vue-loader/lib/plugin' );
 
 module.exports = {
 	module: {
-		rules: [{
+		rules: [
+			{
 				test: /\.vue$/,
 				loader: 'vue-loader',
 				options: {
-					loaders: {}
+					loaders: {},
 					// other vue-loader options go here
-				}
+				},
 			},
 			{
 				test: /\.js$/,
 				loader: 'babel-loader',
-				exclude: /node_modules/
+				exclude: /node_modules/,
 			},
 			{
 				test: /\.css$/,
 				use: [
 					'style-loader',
-					'css-loader'
-				]
+					'css-loader',
+				],
 			},
 			{
 				test: /\.(png|jpg|gif|svg)$/,
 				loader: 'file-loader',
 				options: {
-					name: '[name].[ext]?[hash]'
-				}
-			}
-		]
+					name: '[name].[ext]?[hash]',
+				},
+			},
+		],
 	},
 	resolve: {
 		alias: {
 			'vue$': 'vue/dist/vue.esm.js',
-			'@': path.resolve(__dirname, '../src')
-		}
+			'@': path.resolve( __dirname, '../src' ),
+		},
 	},
-	devtool: '#eval-source-map'
-}
+	devtool: '#eval-source-map',
+	plugins: [
+		// make sure to include the plugin!
+		new VueLoaderPlugin(),
+	],
+};
 
-if (process.env.NODE_ENV === 'production') {
-	module.exports.devtool = '#source-map'
+if ( process.env.NODE_ENV === 'production' ) {
+	module.exports.devtool = '#source-map';
 	// http://vue-loader.vuejs.org/en/workflow/production.html
-	module.exports.plugins = (module.exports.plugins || []).concat([
-		new webpack.DefinePlugin({
+	module.exports.mode = 'production';
+	module.exports.optimization = {
+		minimizer: [
+
+		],
+	};
+	module.exports.plugins = (module.exports.plugins || []).concat( [
+		new webpack.DefinePlugin( {
 			'process.env': {
-				NODE_ENV: '"production"'
-			}
-		}),
-		new webpack.optimize.UglifyJsPlugin({
-			sourceMap: true,
-			compress: {
-				warnings: false
-			}
-		}),
-		new webpack.LoaderOptionsPlugin({
-			minimize: true
-		})
-	])
+				NODE_ENV: '"production"',
+			},
+		} ),
+		new webpack.LoaderOptionsPlugin( {
+			minimize: true,
+		} ),
+	] );
 }
